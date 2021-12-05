496e431489dfd421c888bab94556e3d0c6b50910
seiya <r108338@yahoo.co.jp>
2021-12-05 18:25:56 +0900

    Add typings for collection-pluck

---
+++ packages/collection-pluck/index.d.ts
@@ -0,0 +1,21 @@
+declare function pluck<
+  P extends string | number | symbol,
+  O extends Record<P, unknown>,
+>(collection: O[], propertyName: P): O[P][];
+// declare function pluck<
+//   P extends string | number | symbol,
+//   K extends string | number | symbol,
+//   O extends Record<P, unknown>,
+// >(
+//   collection: Record<K, O>,
+//   propertyName: P,
+// ): {[k in K]: (typeof collection)[k][P]}
+declare function pluck<
+  P extends string | number | symbol,
+  K extends string | number | symbol,
+  C extends {[k in K]: Record<P, unknown>}
+>(
+  collection: C,
+  propertyName: P,
+): {[k in K]: C[k][P]}
+export = pluck;
